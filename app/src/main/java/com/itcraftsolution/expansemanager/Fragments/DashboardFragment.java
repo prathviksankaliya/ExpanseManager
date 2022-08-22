22d767dcd445d0aa554e2aca9a7fae8a88c72aae
Shadow <prathviksankaliya@gmail.com>
2022-08-22 14:24:05 +0530

    Money Progress Bar Added

--- app/src/main/java/com/itcraftsolution/expansemanager/Fragments/DashboardFragment.java
+++ app/src/main/java/com/itcraftsolution/expansemanager/Fragments/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.itcraftsolution.expansemanager.Fragments;
 
+import android.animation.ValueAnimator;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -40,23 +41,44 @@ public class DashboardFragment extends Fragment {
                 getParentFragmentManager().beginTransaction().replace(R.id.frMainContainer, new UserDetailsFragment()).commit();
             }
         });
-        records = new ArrayList<>();
-        records.add(new PieEntry(32, "Food"));
-        records.add(new PieEntry(12, "Subscription"));
-        records.add(new PieEntry(8, "Water"));
-        records.add(new PieEntry(20, "Travel"));
+//        records = new ArrayList<>();
+//        records.add(new PieEntry(32, "Food"));
+//        records.add(new PieEntry(12, "Subscription"));
+//        records.add(new PieEntry(8, "Water"));
+//        records.add(new PieEntry(20, "Travel"));
+//
+//        PieDataSet pieDataSet = new PieDataSet(records, "Pie Chart");
+//        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+//        pieDataSet.setValueTextColor(Color.BLACK);
+//        pieDataSet.setValueTextSize(22);
+//
+//        PieData pieData = new PieData(pieDataSet);
+//        binding.dashPieChart.setData(pieData);
+//        binding.dashPieChart.getDescription().setEnabled(true);
+//        binding.dashPieChart.setCenterText("Income Chart");
+//        binding.dashPieChart.animate();
 
-        PieDataSet pieDataSet = new PieDataSet(records, "Pie Chart");
-        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-        pieDataSet.setValueTextColor(Color.BLACK);
-        pieDataSet.setValueTextSize(22);
-
-        PieData pieData = new PieData(pieDataSet);
-        binding.dashPieChart.setData(pieData);
-        binding.dashPieChart.getDescription().setEnabled(true);
-        binding.dashPieChart.setCenterText("Income Chart");
-        binding.dashPieChart.animate();
+            startAnimationCounter(0,67);
 
         return binding.getRoot();
+    }
+
+    private void startAnimationCounter(int start, int end )
+    {
+        ValueAnimator animator = ValueAnimator.ofInt(start, end);
+        animator.setDuration(5000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                binding.txProgressDigit.setText(String.valueOf(valueAnimator.getAnimatedValue().toString() + "%"));
+                binding.pbDashMoney.setProgress(Integer.parseInt(valueAnimator.getAnimatedValue().toString()));
+            }
+        });
+        animator.start();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
